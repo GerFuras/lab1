aafcc1c288d5e8f3d589093c4a0ac281fcc6684e
Geranis Furas <geranisf@gmail.com>
2016-03-25 22:07:00 +0600

    Queston lane

--- main.c
+++ main.c
@@ -2,7 +2,8 @@
 
 int main() {
 	printf("Hello, World\n");
-	printf("Something happens in this World\n")
+	printf("Something happens in this World\n");
+	printf("Who are you in this World?\n");
 	printf("Goodbye, World\n");
 	return 0;
 }
